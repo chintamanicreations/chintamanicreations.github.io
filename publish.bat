62fca3f9affc636ecb631d250d3a4551585de72c
Shashank Ajmera <shashanksajmera@gmail.com>
2023-06-25 14:45:15 +0530

    added favicon

--- publish.bat
+++ publish.bat
@@ -1,4 +1,4 @@
-call ng build --output-path docs --base-href --cname=chintamanicreations.in
+call ng build --output-path docs --base-href
 @echo "Publishing site"
 call git add .
 call git commit -m %1
