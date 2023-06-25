80dd73d944d2637bf1e1f72dc79229107e9b66de
Shashank Ajmera <shashanksajmera@gmail.com>
2023-06-25 14:03:07 +0530

    added favicon

--- src/app/section/category/categories.ts
+++ src/app/section/category/categories.ts
@@ -8,4 +8,60 @@ export class Categories{
         samples: ['logo.jpg','logo.jpg','logo.jpg','logo.jpg','logo.jpg',],
         route: "logo-design"
     }
+
+    static FlyerDesign: Category = {
+        name: "Flyer Design",
+        description: "Capture attention and drive results with our expert Flyer Designing service. Our captivating visuals and persuasive messaging create eye-catching flyers that leave a lasting impact on your target audience.",
+        image: "",
+        samples: [],
+        route: ""
+    }
+
+    static BrochureDesign: Category = {
+        name: "Brochure Design",
+        description: "Make a lasting impression with our expert Brochure Design service. Our team crafts visually stunning and engaging brochures that effectively communicate your message and captivate your target audience.",
+        image: "",
+        samples: [],
+        route: ""
+    }
+
+    static OfficeStationaryDesign : Category = {
+        name: "Office Stationary",
+        description: "Enhance your professional image with our comprehensive Office Stationery service. From eye-catching calendars and folders to impeccably designed business cards and letterheads, we ensure your brand stands out with every interaction.",
+        image: "",
+        samples: [],
+        route: ""
+    }
+
+    static BookPrinting : Category = {
+        name: "Book Printing",
+        description: "Experience exceptional Book and Magazine Printing services. Our business offers professional printing solutions, delivering outstanding quality, vibrant colors, and precise finishing to bring your publications to life.",
+        image: "",
+        samples: [],
+        route: ""
+    }
+
+    static PosterDesign : Category = {
+        name: "Poster Design",
+        description: "Unleash your creativity with our dynamic Poster Design service. Our talented team crafts visually striking and attention-grabbing posters that effectively convey your message and captivate your audience. Make a statement with our exceptional poster designs.",
+        image: "",
+        samples: [],
+        route: ""
+    }
+
+    static WebAndAppDesign : Category = {
+        name: "Web and App Design",
+        description: "Transform your digital presence with our cutting-edge Web and App Designing service. Our skilled team creates stunning user interfaces and seamless experiences that engage your audience, drive conversions, and elevate your online brand.",
+        image: "",
+        samples: [],
+        route: ""
+    }
+
+    static InvitationCardDesign : Category = {
+        name: "Invitation Cards",
+        description: "Create unforgettable moments with our exquisite Invitation Card Design service. From elegant wedding cards to personalized invitations, our team crafts stunning designs that capture the essence of your special occasion and leave a lasting impression on your guests.",
+        image: "",
+        samples: [],
+        route: ""
+    }
 }
